b0d4089d8c754647c6a3e2fd079b6fc2645d396c
simran1622 <simrankour8491@gmail.com>
2023-04-28 17:46:04 +0530

    practice

---
+++ practice1/web.js
@@ -0,0 +1,24 @@
+function getCount(i)
+{
+    let count= Number.parseInt(document.getElementById("count").value);
+    if(i==1)
+    {
+        count=count+1;
+        document.getElementById("count").value=count;
+    }
+    else if(i==2)
+    {
+        if(count>0)
+        count--;
+        document.getElementById("count").value=count;
+    }
+    
+    else
+
+    {
+        count=0;
+        document.getElementById("count").value=count;
+    }
+    
+
+}
